741aa9a9b469a35e6a515f5ae6e9f014a37e4620
HarapriyaSatapathy <harapriya149@gmail.com>
2024-01-30 00:05:10 +0530

    Latest code

---
+++ src/Lab001_Summation.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Lab001_Summation {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
