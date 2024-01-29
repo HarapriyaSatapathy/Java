879d9736ab21f3786076cd09294eac2af84781d1
HarapriyaSatapathy <harapriya149@gmail.com>
2024-01-30 00:42:38 +0530

    Latest code

---
+++ src/Lab003_ImplicitTypeCasting.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Lab003_ImplicitTypeCasting {
+    public static void main(String[] args) {
+        byte a=100;
+        int b=(int)a;
+        // or you can simply write int b=a;
+        System.out.println(b);
+
+
+
+    }
+}
---
+++ src/Lab004_ExplicitTypeCasting.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Lab004_ExplicitTypeCasting {
+    public static void main(String[] args) {
+        int a=300;
+        byte b=(byte)a;
+        System.out.println(b);
+    }
+}
