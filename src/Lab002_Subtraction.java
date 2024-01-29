38c15c7bd13e85478606bca091f365cc38faf42d
HarapriyaSatapathy <harapriya149@gmail.com>
2024-01-30 00:15:05 +0530

    Latest code

---
+++ src/Lab002_Subtraction.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Lab002_Subtraction {
+    public static void main(String[] args) {
+        int a=15;
+        int b=5;
+        int c= a-b;
+        System.out.println("Subtraction is" + " "+c);
+    }
+}
