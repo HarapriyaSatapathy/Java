949f76309b2a592e53950da1aebb6be5b0be7a6f
HarapriyaSatapathy <harapriya149@gmail.com>
2024-01-30 23:15:12 +0530

    Latest code

---
+++ src/Lab005_Concatenation.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Lab005_Concatenation {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        String name="Rinky";
+        System.out.println(a+b+name);
+        //Execution happens from left to right, so in this case 1st 2 int values
+        //addition will be happening and then int+string= string
+        //so it will be 5+10+name=15Rinky
+    }
+}
---
+++ src/Lab006_Concatenation2.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Lab006_Concatenation2 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        String name="Rinky";
+        System.out.println(name+a+b);
+        //Execution happens from left to right, so in this case 1st string+int=string
+         //and then string + one more int=string
+        //so it will be name+5+10=Rinky510
+    }
+}
