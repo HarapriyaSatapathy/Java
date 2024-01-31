52dda8aeaba46687ad7cff9ca8afb3100e2cb501
HarapriyaSatapathy <harapriya149@gmail.com>
2024-02-01 00:00:34 +0530

    Latest code

---
+++ src/oops/Lab001_FunctionCreation.java
@@ -0,0 +1,15 @@
+package src.oops;
+
+public class Lab001_FunctionCreation {
+    public static void main(String[] args) {
+
+    int a= sum(3,4);
+        System.out.println(a);
+    }
+//Create a function syntax= access Modifier returntype methodname (parameterlist){
+//Method body                    }
+    public static int sum(int input1,int input2){
+        return input1+input2;
+    }
+
+}
---
+++ src/oops/Lab002_FunctionCreation2.java
@@ -0,0 +1,13 @@
+package src.oops;
+
+public class Lab002_FunctionCreation2 {
+    public static void main(String[] args) {
+
+        float f=sub(15.5f,10.2f);
+        System.out.println(f);
+
+    }
+    public static float sub(float a,float b){
+        return a-b;
+    }
+}
