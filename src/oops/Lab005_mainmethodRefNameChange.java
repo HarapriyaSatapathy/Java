9e3b67bf473deec4a99c7dac0ddc509a2c8f4f37
HarapriyaSatapathy <harapriya149@gmail.com>
2024-02-01 02:06:37 +0530

    Latest code

---
+++ src/oops/Lab005_mainmethodRefNameChange.java
@@ -0,0 +1,13 @@
+package src.oops;
+
+public class Lab005_mainmethodRefNameChange {
+    //public static void main(String[] args)-The args her is just
+    //a reference name , we can have any value,the program
+    //will run fine
+
+
+    public static void main(String[] Rinky) {
+        System.out.println("Program will run fine!!");
+;
+    }
+}
