b145d395e431eb19e496d56a938f71c91949f264
HarapriyaSatapathy <harapriya149@gmail.com>
2024-02-01 01:27:25 +0530

    Latest code

---
+++ src/oops/Lab003_FunctionCreation3.java
@@ -0,0 +1,26 @@
+package src.oops;
+
+public class Lab003_FunctionCreation3 {
+    public static void main(String[] args) {
+
+        int x= main(10);
+        System.out.println(x);
+
+        String y=main("Rinky");
+        System.out.println(y);
+
+        float z=main(10.5f);
+        System.out.println(z);
+
+    }
+    public static String main(String a){
+        return a;
+
+    }
+    public static int main(int b){
+        return b;
+    }
+    public static float main(float c){
+        return c;
+    }
+}
---
+++ src/oops/Lab004_MethodOverloading1.java
@@ -0,0 +1,32 @@
+package src.oops;
+
+public class Lab004_MethodOverloading1 {
+    public static void main(String[] args) {
+        int a = main(3, 4);
+        System.out.println(a);
+
+        float s=main(10.7f);
+        System.out.println(s);
+
+
+    }
+
+    public static int main(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static void main(String s) {
+        System.out.println("Rinky");
+    }
+
+    public static void main(int num) {
+        num = 10;
+        System.out.println(num);
+    }
+
+
+    public static float main(float s) {
+        return s;
+
+    }
+}
