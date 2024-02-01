6684069fd685beedb7dcfae54f269b299977015d
HarapriyaSatapathy <harapriya149@gmail.com>
2024-02-01 22:15:43 +0530

    Latest Code

---
+++ src/Basics01/Lab007_Devision.java
@@ -0,0 +1,10 @@
+package src.Basics01;
+
+public class Lab007_Devision {
+    public static void main(String[] args) {
+        int a =100;
+        int b=20;
+        int c= a/b;
+        System.out.println(c);
+    }
+}
