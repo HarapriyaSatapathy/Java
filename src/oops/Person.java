7de9cb0164571772a86d78caded649cb78432df5
HarapriyaSatapathy <harapriya149@gmail.com>
2024-02-03 21:53:30 +0530

    Latest code

---
+++ src/oops/Person.java
@@ -0,0 +1,23 @@
+package src.oops;
+
+public class PersonClass {
+    String name;
+    String color;
+    int age;
+    double height;
+    //above all parameters are nothing but the atrributes of a class
+    //And below are teh behaviour/methods of  a class
+
+    void walk() {
+        System.out.println("Walk!!");
+    }
+
+    void eat() {
+        System.out.println("eat");
+    }
+
+    String PrintDetails(String s) {
+        return s;
+    }
+
+}
